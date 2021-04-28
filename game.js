130e8f3b93cfbdaec6a30b116d422e8bed409602
Tejas Bavaskar <44202599+TejasBavaskar@users.noreply.github.com>
2021-04-28 17:10:47 +0530

    feat(snake-death): add snake death logic (#6)

--- game.js
+++ game.js
@@ -1,10 +1,32 @@
-import { SNAKE_SPEED, updateSnake, drawSnake } from './snake.js';
+import { SNAKE_SPEED, updateSnake, drawSnake, getSnakeHead, isSnakeIntersect } from './snake.js';
 import { updateFood, drawFood } from './food.js';
+import { isSnakeOutsideGride } from './grid.js';
 
 let lastRenderTime = 0;
+let isGameOver = false;
 const gameBoard = document.getElementById('board');
 
+startGame();
+
+function startGame() {
+  main();
+}
+
+
 function main(currentTime) {
+  if(isGameOver) {
+    document.querySelector('[data-lost-text]').innerText = `You Lost!!`;
+
+    const lostElement = document.querySelector('.lost-msg');
+    lostElement.classList.add('show');
+
+    const restartBtn = document.getElementById('restart-btn');
+    //isGameOver = false;
+    restartBtn.addEventListener('click', () => {
+      window.location = '/';
+    });
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
 
@@ -16,6 +38,7 @@ function main(currentTime) {
 
   update();
   drawItems();
+  checkDeath();
 }
 
 window.requestAnimationFrame(main);
@@ -29,4 +52,9 @@ function drawItems() {
   gameBoard.innerHTML = '';
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  console.log(isSnakeIntersect());
+  isGameOver = isSnakeOutsideGride(getSnakeHead()) || isSnakeIntersect();
 }
